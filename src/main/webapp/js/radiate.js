fc331ba2f56572fbf1fee5636c7cbc6edb1a03f5
Russell Wilson <russell.hugh.wilson@gmail.com>
2011-11-16 00:44:51 +0000

    Broke javascript down into bite sized methods and made statuses be sorted by state and timestamp descending.

--- src/main/webapp/js/radiate.js
+++ src/main/webapp/js/radiate.js
@@ -1,47 +1,13 @@
 function refresh(view) {
 
-    $.getJSON('/view/' + view + '/feed', function(data) {
+    $.getJSON('/view/' + view + '/feed', function(statuses) {
 
         var jobs = $('<ol/>', { id: 'jobs' });
 
-        $.each(data, function(key, status) {
+        statuses = statuses.sort(statusComparator);
 
-            var job = $('<li/>', { id: status.name, 'class': status.state });
-
-            var heading = $('<h2>' + status.name + '</h2>');
-            job.append(heading);
-
-
-            var timestamp = $('<div/>', { 'class': 'timestamp' })
-            if (status.timestamp != 0) {
-                var date = new Date(status.timestamp);
-                timestamp.append(date.toString());
-            }
-            job.append(timestamp);
-
-            var duration = $('<div/>', { 'class': 'duration' })
-            if (status.duration != 0) {
-                duration.append(makeDuration(status.duration));
-            }
-            job.append(duration);
-
-            var causes = $('<ol/>', { 'class': 'causes' });
-            $.each(status.causes, function(key, cause) {
-                causes.append($('<li/>').append(cause));
-            });
-            job.append(causes);
-
-            var changes = $('<ol/>', { 'class': 'changes' });
-            if (status.changes.length == 0) {
-                var element = $('<li/>').append('No SCM changes');
-                changes.append(element);
-            } else {
-                $.each(status.changes, function(key, change) {
-                    changes.append($('<li/>').append(change));
-                });
-            }
-            job.append(changes);
-
+        $.each(statuses, function(key, status) {
+            var job = buildStatusMarkup(status);
             jobs.append(job);
         });
 
@@ -49,8 +15,103 @@ function refresh(view) {
         $('body').append(jobs);
 
         var invokation = "refresh('" + view + "')";
-        setTimeout(invokation, 5000);
+        setTimeout(invokation, 2000);
     });
+}
+
+function buildStatusMarkup(status) {
+
+    var job = $('<li/>', { id: status.name, 'class': status.state });
+
+    var name = buildNameMarkup(status);
+    job.append(name);
+
+    var timestamp = buildTimestampMarkup(status);
+    job.append(timestamp);
+
+    var duration = buildDurationMarkup(status);
+    job.append(duration);
+
+    var causes = buildCausesMarkup(status);
+    job.append(causes);
+
+    var changes = buildChangesMarkup(status);
+    job.append(changes);
+
+    return job;
+}
+
+function buildNameMarkup(status) {
+    return $('<h2>' + status.name + '</h2>');
+}
+
+function buildTimestampMarkup(status) {
+
+    var timestamp = $('<div/>', { 'class': 'timestamp' })
+    if (status.timestamp != 0) {
+        var date = new Date(status.timestamp);
+        timestamp.append(date.toString());
+    }
+
+    return timestamp;
+}
+
+function buildDurationMarkup(status) {
+
+    var duration = $('<div/>', { 'class': 'duration' })
+    if (status.duration != 0) {
+        duration.append(makeDuration(status.duration));
+    }
+
+    return duration;
+}
+
+function buildCausesMarkup(status) {
+
+    var causes = $('<ol/>', { 'class': 'causes' });
+    $.each(status.causes, function(key, cause) {
+        causes.append($('<li/>').append(cause));
+    });
+
+    return causes;
+}
+
+function buildChangesMarkup(status) {
+
+    var changes = $('<ol/>', { 'class': 'changes' });
+    if (status.changes.length == 0) {
+        var element = $('<li/>').append('No SCM changes');
+        changes.append(element);
+    } else {
+        $.each(status.changes, function(key, change) {
+            changes.append($('<li/>').append(change));
+        });
+    }
+
+    return changes;
+}
+
+function statusComparator(s1, s2) {
+
+    if (s1.state == 'building' && s2.state != 'building') { return -1; }
+    if (s1.state != 'building' && s2.state == 'building') { return  1; }
+
+    if (s1.state == 'failure'  && s2.state != 'failure')  { return -1; }
+    if (s1.state != 'failure'  && s2.state == 'failure')  { return  1; }
+
+    if (s1.state == 'aborted'  && s2.state != 'aborted')  { return -1; }
+    if (s1.state != 'aborted'  && s2.state == 'aborted')  { return  1; }
+
+    if (s1.state == 'unstable' && s2.state != 'unstable') { return -1; }
+    if (s1.state != 'unstable' && s2.state == 'unstable') { return  1; }
+
+    if (s1.state == 'notbuilt' && s2.state != 'notbuilt') { return -1; }
+    if (s1.state != 'notbuilt' && s2.state == 'notbuilt') { return  1; }
+
+    if (s1.state == 'unknown'  && s2.state != 'unknown')  { return -1; }
+    if (s1.state != 'unknown'  && s2.state == 'unknown')  { return  1; }
+
+    return s2.timestamp - s1.timestamp;
 }
 
 function makeDuration(milliseconds) {
